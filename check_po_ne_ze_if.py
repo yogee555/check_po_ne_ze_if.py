2054f8ab5e7657303734584bc5ce232b641a9289
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-02-02 12:08:22 +0530

    Add files via upload

---
+++ check_po_ne_ze_if.py
@@ -0,0 +1,8 @@
+# check positive, negative or zero
+num=float(input("Enter a number: "))
+if num>0:
+    print("positive number")
+elif num==0:
+    print("zero")
+else:
+    print("Negative number")
